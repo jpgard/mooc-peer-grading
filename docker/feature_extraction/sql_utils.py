f0c0bf1b69914e2356bfe608267dc32163da8609
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-12-19 15:26:36 -0800

    modified from original forum post workflow to peer grading; queries all hg_assessment tables into separate csv files

--- docker/feature_extraction/sql_utils.py
+++ docker/feature_extraction/sql_utils.py
@@ -3,7 +3,6 @@ import subprocess
 import shutil
 import pandas as pd
 
-
 DATABASE_NAME = "course"
 
 
@@ -46,7 +45,7 @@ def initialize_database(database_name=DATABASE_NAME):
     return
 
 
-def extract_coursera_sql_data(course, session, outfile):
+def extract_coursera_sql_data(course, session):
     """
     Initialize the mySQL database, load files, and execute queries to deposit csv files of data into /input/course/session directory.
     :param course: course name.
@@ -58,15 +57,22 @@ def extract_coursera_sql_data(course, session, outfile):
     course_session_dir = os.path.join("/input", course, session)
     mysql_default_output_dir = "/var/lib/mysql/{}/".format(
         DATABASE_NAME)  # this is the only location mysql can write to
-    outfile_temp_fp = os.path.join(mysql_default_output_dir, outfile)
-    outfile_fp = os.path.join(course_session_dir, outfile)
     hash_mapping_sql_dump = \
-        [x for x in os.listdir(course_session_dir) if "anonymized_general" in x and session in x][0] # contains users table
+        [x for x in os.listdir(course_session_dir) if "anonymized_general" in x and session in x][
+            0]  # contains users table
     initialize_database()
     load_mysql_dump(os.path.join(course_session_dir, hash_mapping_sql_dump))
     # execute forum comment query and send to csv
-    query = """SELECT * FROM quiz_submission_metadata WHERE item_id IN (SELECT id FROM quiz_metadata WHERE quiz_type = 'quiz' AND deleted = 0 AND parent_id = -1) AND session_user_id IN (SELECT session_user_id FROM users WHERE access_group_id = 4)"""
-    execute_mysql_query_into_csv(query, file=outfile_temp_fp)
-    # move both files to intended location -- this is a hack but it works without needing to chance mysql permissions
-    shutil.move(outfile_temp_fp, outfile_fp)
+    for tablename in ["hg_assessment_metadata", "hg_assessment_submission_metadata",
+                      "hg_assessment_overall_evaluation_metadata", "hg_assessment_evaluation_metadata",
+                      "hg_assessment_calibration_gradings", "hg_assessment_peer_grading_metadata",
+                      "hg_assessment_peer_grading_set_metadata", "hg_assessment_self_grading_set_metadata",
+                      "hg_assessment_training_metadata", "hg_assessment_training_set_metadata"
+                      ]:
+        query = """SELECT * FROM {}""".format(tablename)
+        outfile = tablename + ".csv"
+        outfile_temp_fp = os.path.join(mysql_default_output_dir, outfile)
+        outfile_fp = os.path.join(course_session_dir, outfile)
+        execute_mysql_query_into_csv(query, file=outfile_temp_fp)
+        shutil.move(outfile_temp_fp, outfile_fp)
     return
